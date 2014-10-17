4b84ee348893f7a2b84e17ac51c69d63ec68a610
Axxx <axxx@wilson>
2014-10-17 15:57:40 +0200

    Allow personal.lua parameter personalization

--- rc.lua
+++ rc.lua
@@ -107,6 +107,8 @@ editor_cmd = terminal .. " -e " .. editor
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
+-- Allow personal.lua file to overload some settingsS
+require('personal')
 -- Table of layouts to cover with awful.layout.inc, order matters.
 
 local layouts =
