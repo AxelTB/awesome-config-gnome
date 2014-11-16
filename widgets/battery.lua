db3bcd09ddf13e2c94df27196763dc04d4ea9f93
AxelTB <axel99999@hotmail.com>
2014-11-16 03:01:36 +0100

    Battery dbus version

--- widgets/battery.lua
+++ widgets/battery.lua
@@ -21,9 +21,12 @@ local batStatus={state,rate,fullDesign,fullReal}
 local battery_state = {
     ["Full"]        = "↯", ["Unknown"]     = "?",
     ["Charged"]     = "↯", ["Charging"]    = "⌁",
-    ["Discharging"] = "",  ["Empty"] = "x"
+    ["Discharging"] = "",  ["Empty"] = "x",
+    ["PCharge"] = ".", ["PDischage"] = ",",
 }
 
+local dbusState_lookup = {"Charging","Discharging","Empty","Charged","PCharge","PDischage"}
+dbusState_lookup[0]="Unknown"
 
 local function set_value(self,value)
     self._value = value
@@ -48,63 +51,59 @@ local function draw(self,w,cr,width,height)
     cr:fill()
     self._tooltip.text = ((self._value or 0)*100)..'%'
     cr:set_source_rgba(1,0,0,1)
-    cr:set_font_size(30)
+    cr:set_font_size(15)
     local extents = cr:text_extents(battery_state[batStatus.state or "Unknown"] or '*')
     cr:move_to(ratio+(width-4*ratio)/2-extents.width/2,height/2+extents.height)
     cr:show_text(battery_state[batStatus.state or "Unknown"] or '*')
     cr:restore()
 end
 
---Parses data from acpi and return the batStatus table
-local function parseAcpi()
-    local Status={state,rate,fullDesign,fullReal}
-    --Parse initial data
-    local pipe0 = io.popen("acpi -V | grep 'Battery "..batConfig.batId.."..'| cut -d':' -f2")
-    local buffer = pipe0:read("*all")
-    pipe0:close()
 
-    if #buffer == 0 then
-        print("No Battery")
-        Status.state="Empty"
-    else
-        local data=buffer:split(",")
-        if #data ~= 4 then print("Acpi output parsing problem:'",buffer,"'")
-        else
-            Status.state=data[1]:match("%a+")
-            Status.rate=string.match(data[2],"%d+")
-            Status.fullDesign=string.match(data[3],"%d+ mAh"):match("%d+")
-            Status.fullReal=string.match(data[4],"%d+ mAh"):match("%d+")
-            --print("BatStatus:\n\tState: '"..batStatus.state.."'\n\tRate: "..batStatus.rate.."\n\tFullD:"..batStatus.fullDesign.."\n\tFullR:"..batStatus.fullReal)
-        end
-    end
 
-    return Status
-end
 
-local function timeout(wdg)
-    batStatus=parseAcpi()
-    if batStatus.state == "Empty" then
-        --AXTODO: Signal no battery and reduce rate of update 
-    end
-    wdg:set_value((tonumber(batStatus.rate) or 0)/100)
-    wdg:emit_signal("widget::updated")
-end
+
 
 ---args={ battery_id, update_time}
 ---
 ---     battery_id: number of the visualized battery (Default 0)
 ---     update_time:    w idget update time in seconds (Default 15)
 local function new(args)
+    local ib = wibox.widget.base.empty_widget()
+
+    --Update from dbus with wirefu
+    local function updateDBusAsync()
+        wirefu.SYSTEM.org.freedesktop.UPower("/org/freedesktop/UPower/devices/battery_BAT0").org.freedesktop.UPower.Device.State : get( function (work)
+                
+                if work ~= nil then
+                    batStatus.state=dbusState_lookup[work]
+                    print("BatState:",batStatus.state)
+                    ib:emit_signal("widget::updated")
+                end
+            end,function(err) print("ERR:",err) end)
+        
+        wirefu.SYSTEM.org.freedesktop.UPower("/org/freedesktop/UPower/devices/battery_BAT0").org.freedesktop.UPower.Device.Percentage : get(function (percentage)
+                print("Percentage:",percentage)
+                ib._value = ((tonumber(percentage) or 0)/100)
+                ib:emit_signal("widget::updated")
+            end,function(err) print("ERR:",err) end)
+    end
+    local function timeout(wdg)
+        updateDBusAsync()
+        --batStatus=parseAcpi()
+        if batStatus.state == "Empty" then
+            --AXTODO: Signal no battery and reduce rate of update 
+        end
+        --wdg:set_value((tonumber(batStatus.rate) or 0)/100)
+        --wdg:emit_signal("widget::updated")
+    end
+
     --If any argument parse 'em
     if args~=nil then
         batConfig.batId = args.battery_id or batConfig.batId
         batConfig.batTimeout = args.update_time or batConfig.batTimeout
     end
 
-    --Parse initial data
-    batStatus=parseAcpi()
 
-    local ib = wibox.widget.base.empty_widget()
 
 
     ib.set_value = set_value
