da1230b210453ad788163e7b6f664c10011f8749
AxelTB <axel99999@hotmail.com>
2014-11-28 00:04:43 +0100

    DBUS battery widget restored

--- widgets/battery.lua
+++ widgets/battery.lua
@@ -7,98 +7,114 @@ local gio       = require( "lgi"                      ).Gio
 local wibox     = require( "wibox"                    )
 local beautiful = require( "beautiful"                )
 
+
+local lgi  = require     'lgi'
+local wirefu = require("wirefu")
+local GLib = lgi.require 'GLib'
+
 local capi = {timer=timer}
 
+--Save config
+local batConfig={batId=0,batTimeout=15}
+local batStatus={state,rate,fullDesign,fullReal}
+
 local battery_state = {
-  ["Full\n"]        = "↯", ["Unknown\n"]     = "?",
-  ["Charged\n"]     = "↯", ["Charging\n"]    = "⌁",
-  ["Discharging\n"] = ""
+    ["Full"]        = "↯", ["Unknown"]     = "?",
+    ["Charged"]     = "↯", ["Charging"]    = "⌁",
+    ["Discharging"] = "",  ["Empty"] = "x",
+    ["PCharge"] = ".", ["PDischage"] = ",",
 }
 
-local full_energy,bat_name,current_status = 0,"",""
+local dbusState_lookup = {"Charging","Discharging","Empty","Charged","PCharge","PDischage"}
+dbusState_lookup[0]="Unknown"
 
 local function set_value(self,value)
-  self._value = value
-  self:emit_signal("widget::updated")
+    self._value = value
+    self:emit_signal("widget::updated")
 end
 
 local function fit(self,width,height)
-  return width > (height * 1.5) and (height * 1.5) or width,height
+    return width > (height * 1.5) and (height * 1.5) or width,height
 end
 
 local function draw(self,w,cr,width,height)
-  cr:save()
-  cr:set_source(color(beautiful.icon_grad or beautiful.fg_normal))
-  cr:paint()
-  local ratio = height / 10
-  cr:set_source(color(beautiful.bg_alternate or beautiful.bg_normal))
-  cr:rectangle(ratio,2*ratio,width-4*ratio,height-4*ratio)
-  cr:stroke()
-  cr:rectangle(width-3*ratio,height/3,1.5*ratio,height/3)
-  cr:fill()
-  cr:rectangle(2*ratio,3*ratio,(width-6*ratio)*(self._value or 0),height-6*ratio)
-  cr:fill()
-  self._tooltip.text = ((self._value or 0)*100)..'%'
-  cr:set_source_rgba(1,0,0,1)
-  cr:set_font_size(30)
-  local extents = cr:text_extents(battery_state[current_status])
-  cr:move_to(ratio+(width-4*ratio)/2-extents.width/2,height/2+extents.height)
-  cr:show_text(battery_state[current_status])
-  cr:restore()
+    cr:save()
+    cr:set_source(color(beautiful.icon_grad or beautiful.fg_normal))
+    cr:paint()
+    local ratio = height / 10
+    cr:set_source(color(beautiful.bg_alternate or beautiful.bg_normal))
+    cr:rectangle(ratio,2*ratio,width-4*ratio,height-4*ratio)
+    cr:stroke()
+    cr:rectangle(width-3*ratio,height/3,1.5*ratio,height/3)
+    cr:fill()
+    cr:rectangle(2*ratio,3*ratio,(width-6*ratio)*(self._value or 0),height-6*ratio)
+    cr:fill()
+    self._tooltip.text = ((self._value or 0)*100)..'%'
+    cr:set_source_rgba(1,0,0,1)
+    cr:set_font_size(15)
+    local extents = cr:text_extents(battery_state[batStatus.state or "Unknown"] or '*')
+    cr:move_to(ratio+(width-4*ratio)/2-extents.width/2,height/2+extents.height)
+    cr:show_text(battery_state[batStatus.state or "Unknown"] or '*')
+    cr:restore()
 end
 
-local function check_present(name)
-  local f = io.open('/sys/class/power_supply/'..name..'/present','r')
-  if f then f:close() end
-  return f ~= nil
-end
 
-local function timeout(wdg)
-  gio.File.new_for_path('/sys/class/power_supply/'..(bat_name)..'/energy_now'):load_contents_async(nil,function(file,task,c)
-      local content = file:load_contents_finish(task)
-      if content then
-        local now = tonumber(tostring(content))
-        local percent = now/full_energy
-        percent = math.floor(percent* 100)/100
-        wdg:set_value(percent)
-      end
-  end)
-  gio.File.new_for_path('/sys/class/power_supply/'..(bat_name)..'/status'):load_contents_async(nil,function(file,task,c)
-      local content = file:load_contents_finish(task)
-      if content then
-        local str = tostring(content)
-        if current_status ~= str then
-          current_status = str
-          wdg:emit_signal("widget::updated")
-        end
-      end
-  end)
-end
 
+
+
+
+---args={ battery_id, update_time}
+---
+---     battery_id: number of the visualized battery (Default 0)
+---     update_time:    w idget update time in seconds (Default 15)
 local function new(args)
-  local args = args or {}
-  local ib = wibox.widget.base.empty_widget()
-  bat_name = args.name or "BAT0"
-  if check_present(bat_name) then
+    local ib = wibox.widget.base.empty_widget()
 
-    -- Check try to load the full energy value
-    gio.File.new_for_path('/sys/class/power_supply/'..(bat_name)..'/energy_full'):load_contents_async(nil,function(file,task,c)
-        local content = file:load_contents_finish(task)
-        if content then
-            full_energy = tonumber(tostring(content))
+    --Update from dbus with wirefu
+    local function updateDBusAsync()
+        wirefu.SYSTEM.org.freedesktop.UPower("/org/freedesktop/UPower/devices/battery_BAT0").org.freedesktop.UPower.Device.State : get( function (work)
+                
+                if work ~= nil then
+                    batStatus.state=dbusState_lookup[work]
+                    print("BatState:",batStatus.state)
+                    ib:emit_signal("widget::updated")
+                end
+            end,function(err) print("ERR:",err) end)
+        
+        wirefu.SYSTEM.org.freedesktop.UPower("/org/freedesktop/UPower/devices/battery_BAT0").org.freedesktop.UPower.Device.Percentage : get(function (percentage)
+                print("Percentage:",percentage)
+                ib._value = ((tonumber(percentage) or 0)/100)
+                ib:emit_signal("widget::updated")
+            end,function(err) print("ERR:",err) end)
+    end
+    local function timeout(wdg)
+        updateDBusAsync()
+        --batStatus=parseAcpi()
+        if batStatus.state == "Empty" then
+            --AXTODO: Signal no battery and reduce rate of update 
         end
-    end)
+        --wdg:set_value((tonumber(batStatus.rate) or 0)/100)
+        --wdg:emit_signal("widget::updated")
+    end
+
+    --If any argument parse 'em
+    if args~=nil then
+        batConfig.batId = args.battery_id or batConfig.batId
+        batConfig.batTimeout = args.update_time or batConfig.batTimeout
+    end
+
+
+
 
     ib.set_value = set_value
     ib.fit=fit
     ib.draw = draw
     ib:set_tooltip("100%")
-    local t = capi.timer({timeout=15})
+    local t = capi.timer({timeout=batConfig.batTimeout})
     t:connect_signal("timeout",function() timeout(ib) end)
     t:start()
     timeout(ib)
-  end
-  return ib
+    return ib
 end
 
 return setmetatable({}, { __call = function(_, ...) return new(...) end })
